4a7160088ceaa04978555fca5746eaf97f20af9d
Ben Belczak <benbelczak33@gmail.com>
2023-11-17 03:50:05 -0600

    changes to the app.py file

--- app.py
+++ app.py
@@ -27,14 +27,57 @@ station = Base.classes.station
 
 # Create our session (link) from Python to the DB
 
-
 #################################################
 # Flask Setup
 #################################################
 app = Flask(__name__)
 
-
-
 #################################################
 # Flask Routes
 #################################################
+
+@app.route("/")
+def welcome():
+    return(
+        f"Available Routes:<br/>"
+        f"/api/v1.0/precipitation<br/>"
+        f"/api/v1.0/stations<br/>"
+        f"/api/v1.0/tobs<br/>"
+        f"/api/v1.0/<start>"
+        f"/api/v1.0/<start>/<end>"
+    )
+
+@app.route("/api/v1.0/precipitation")
+def precipitation():
+
+    session = Session(engine)
+
+    session.close()
+
+@app.route("/api/v1.0/stations")
+def stations():
+
+    session = Session(engine)
+
+    station_name = session.query(measurement.station).\
+    group_by(measurement.station).all()
+
+    session.close()
+
+    return jsonify(station_name)
+
+@app.route("/api/v1.0/tobs")
+def tobs():
+
+    session = Session(engine)
+
+    session.close()
+
+@app.route("/api/v1.0/<start>")
+def start():
+
+@app.route("/api/v1.0/<start>/<end>")
+def end():
+
+if __name__ == '__main__':
+    app.run(debug=True)
